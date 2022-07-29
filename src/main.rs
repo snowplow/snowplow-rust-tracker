9d8f694b39a79ba991d87fa2eae82332ead66960
Greg Leonard <45019882+greg-el@users.noreply.github.com>
2022-07-29 18:46:03 +0100

    Update main to use tracking functions

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,42 @@
 use serde_json::json;
 use snowplow_rust_tracker::snowplow::emitter::Emitter;
-use snowplow_rust_tracker::snowplow::event::SelfDescribingJson;
+use snowplow_rust_tracker::snowplow::event::ScreenViewEvent;
+use snowplow_rust_tracker::snowplow::event::StructuredEvent;
 use snowplow_rust_tracker::snowplow::tracker::Tracker;
-use snowplow_rust_tracker::snowplow::Snowplow;
-// use serde_json::json;
 
 #[tokio::main]
 async fn main() {
-    let mut trackers: Vec<Tracker> = Vec::new();
-    let mut sp = Snowplow::new();
-
-    let y = SelfDescribingJson {
-        schema: "namespace".to_string(),
-        data: json!({
-            "name": "John Doe",
-            "age": 43u32,
-            "phones": [
-                "+44 1234567",
-                "+44 2345678"
-            ]
-        }),
-    };
-
     let emitter = Emitter::new("http://localhost:9090".to_string());
+    let tracker = Tracker::new("namespace".to_string(), "id".to_string(), emitter);
 
-    let x = sp.create_tracker("namespace".to_string(), "id".to_string(), emitter);
+    let self_desc_event = tracker
+        .track_self_describing_event(
+            "iglu:com.snowplowanalytics.snowplow/screen_view/jsonschema/1-0-0".to_string(),
+            json!({"name": "test", "id": "something else"}).to_string(),
+        )
+        .await;
 
-    x.track(y).await;
+    let struct_event = tracker
+        .track_struct_event(
+            StructuredEvent::builder()
+                .category("test".to_string())
+                .action("test_action".to_string())
+                .build()
+                .unwrap(),
+        )
+        .await;
+
+    let screen_view = tracker
+        .track_screen_view(
+            ScreenViewEvent::builder()
+                .id("this is".to_string())
+                .name("a screen view".to_string())
+                .build()
+                .unwrap(),
+        )
+        .await;
+
+    println!("Self Describing Event: {:?}", self_desc_event);
+    println!("Structured Event: {:?}", struct_event);
+    println!("Screen View: {:?}", screen_view);
 }
