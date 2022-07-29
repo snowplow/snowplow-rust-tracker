342124561bea8d01a0dc40aef8b191a351327a5d
Greg Leonard <45019882+greg-el@users.noreply.github.com>
2022-07-29 18:44:07 +0100

    Flesh out Events
    - add builders
    - various serialization tweaks to be in the correct format for the collector

--- src/snowplow/event.rs
+++ src/snowplow/event.rs
@@ -1,4 +1,6 @@
+use derive_builder::Builder;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use serde_json::Value;
 
 #[derive(Serialize, Deserialize)]
@@ -20,30 +22,81 @@ impl Event {
 }
 
 #[derive(Serialize, Deserialize)]
-struct SelfDescribingEvent {
+pub struct SelfDescribingEvent {
     pub event: Event,
     pub json: SelfDescribingJson,
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct SelfDescribingJson {
+pub struct SelfDescribingData {
     pub schema: String,
     pub data: Value,
 }
 
-#[derive(Serialize, Deserialize)]
-struct StructuredEvent {
-    pub event: Event,
-    pub category: String,
-    pub action: String,
-    pub property: String,
-    pub label: String,
-    pub value: u128,
+#[derive(Deserialize)]
+pub struct SelfDescribingJson {
+    pub schema: String,
+    pub data: SelfDescribingData,
 }
 
-#[derive(Serialize, Deserialize)]
+impl SelfDescribingJson {
+    pub fn from_schema_and_data(schema: String, data: String) -> Result<Self, serde_json::Error> {
+        let data: Value = serde_json::from_str(&data)?;
+        Ok(SelfDescribingJson {
+            schema: "iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0"
+                .to_string(),
+            data: SelfDescribingData { schema, data },
+        })
+    }
+}
+
+// The collector expects the `data` field of the `SelfDescribingJson` to be an object,
+// but the SelfDescribingJson to be a string, so we have to manually serialize SelfDescribingJson
+impl Serialize for SelfDescribingJson {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(
+            &json!({
+                "schema": self.schema,
+                "data": self.data,
+            })
+            .to_string(),
+        )
+    }
+}
+
+#[derive(Serialize, Deserialize, Builder)]
+pub struct StructuredEvent {
+    pub category: String,
+    pub action: String,
+    #[builder(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub property: Option<String>,
+    #[builder(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub label: Option<String>,
+    #[builder(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    // serde isn't happy with u128 and I'm not sure why
+    pub value: Option<u64>,
+}
+
+impl StructuredEvent {
+    pub fn builder() -> StructuredEventBuilder {
+        StructuredEventBuilder::default()
+    }
+}
+
+#[derive(Serialize, Deserialize, Builder)]
 pub struct ScreenViewEvent {
-    pub event: Event,
     pub name: String,
     pub id: String,
+}
+
+impl ScreenViewEvent {
+    pub fn builder() -> ScreenViewEventBuilder {
+        ScreenViewEventBuilder::default()
+    }
 }
